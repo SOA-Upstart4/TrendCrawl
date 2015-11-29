1da78cf0c4d02b1044ac7becbb8239d358b80e37
LeeTung-Yu <dfg1021@hotmail.com.tw>
2015-11-30 00:30:40 +0800

    add article view's test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,14 +14,12 @@ describe 'Trend Crawl' do
     it 'finds the title' do
       @browser.title.must_equal 'Explore with FirstGlance'
     end
-  end
 
-  describe 'Go to trend_bs page' do
     it 'finds the header' do
       @browser.h1.text.must_equal 'Explore with FirstGlance'
     end
 
-    it 'finds three tabs' do
+    it 'has three tabs' do
       @browser.li(id: 'function_tab1').text.must_equal 'Trend'
       @browser.li(id: 'function_tab2').text.must_equal 'Article'
       @browser.li(id: 'function_tab3').text.must_equal 'About'
@@ -29,6 +27,22 @@ describe 'Trend Crawl' do
 
     it 'finds keywords area' do
       @browser.h2.text.must_equal 'Keywords'
+    end
+  end
+
+  describe 'Go to trend_bs page' do
+    it 'show dashboard' do
+      @browser.link(text: 'Trend').click
+
+      @browser.h4.text.must_equal 'Dashboard'
+    end
+  end
+
+  describe 'Go to article_bs page' do
+    it 'show article' do
+      @browser.link(text: 'Article').click
+
+      @browser.h4.text.must_equal 'Contents'
     end
   end
 end
