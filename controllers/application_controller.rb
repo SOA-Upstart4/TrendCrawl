12cc8bf78ab1dd289e01e28f3c3e411855dcd603
LeeTung-Yu <dfg1021@hotmail.com.tw>
2016-01-08 00:25:53 +0800

    update x-axis & show past 12 months trend line

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -35,6 +35,7 @@ class ApplicationController < Sinatra::Base
 
     ## show trend line
     @data_count = []
+    @categories = set_xaxis
     @tags = params['tags']
     @author = params['author']
     @title = params['title']
