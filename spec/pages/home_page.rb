729f3bbbc43fdfbf03139d4b9adf45800b2b648a
LeeTung-Yu <dfg1021@hotmail.com.tw>
2016-01-09 17:07:15 +0800

    change text_field index

--- spec/pages/home_page.rb
+++ spec/pages/home_page.rb
@@ -12,7 +12,7 @@ class HomePage
   h2(:keywords_header, text: 'Keywords')
   div(:keywords_div, class: 'col-sm-3')
   div(:plot_div, id: 'plot')
-  text_field(:search_keyword, name: 'added_word')
+  text_field(:search_keyword, id: 'add_keyword_side')
   button(:search_button, id: 'search')
   button(:delete_button, id: 'delete_first')
   unordered_list(:key, id: 'keyword_list')
